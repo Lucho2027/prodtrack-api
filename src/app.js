a57b6cd36c61af83c14f01ff2e4ad1eec55b11d7
Lucho2027 <luis01.ro@gmail.com>
2020-02-04 20:29:17 -0600

    "Third Commit successfully wired simple get service"

--- src/app.js
+++ src/app.js
@@ -1,12 +1,31 @@
-const express = require("express");
-const app = express();
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const helmet = require("helmet")
+const { NODE_ENV } = require("./config")
 
-const PORT = process.env.PORT || 3000;
+const app = express()
 
-app.get("/api/*", (req, res) => {
-  res.json({ ok: true });
-});
+const morganOption = NODE_ENV === "production" ? "tiny" : "common"
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
 
-module.exports = { app };
+app.get("/", (req, res) => {
+	res.send("Hello, world!")
+})
+
+app.use(function errorHandler(error, req, res, next) {
+	let response
+	if (NODE_ENV === "production") {
+		response = { error: { message: "server error" } }
+	} else {
+		console.error(error)
+		response = { message: error.mesage, error }
+	}
+	res.status(500).json(response)
+})
+
+module.exports = app
