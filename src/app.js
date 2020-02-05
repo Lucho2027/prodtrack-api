e21783ba8e0f58a3ee68dced9f69ad0ccfdb719b
Luis Rodriguez <luis01.ro@gmail.com>
2020-02-04 20:12:44 -0600

    "Second Commit "

--- src/app.js
+++ src/app.js
@@ -1,34 +1,12 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const express = require("express");
+const app = express();
 
-const app = express()
+const PORT = process.env.PORT || 3000;
 
-const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
+app.get("/api/*", (req, res) => {
+  res.json({ ok: true });
+});
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-app.get('/',(req,res)=>{
-    res.send('Hello, world!')
-})
-
-app.use(function errorHandler(error,req,res,next) {
-    let response 
-    if (NODE_ENV ==='production') {
-        response = {error: { message: 'server error'}}
-    
-    } else {
-        console.error(error)
-        response = { message:error.mesage, error}
-      
-    }
-    res.status(500).json(response)
-
-})
-
-module.exports=app
+module.exports = { app };
